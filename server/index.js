d096190fc9fd224a87d01cd039d2189215cbe724
Ben Eccles <ben.g.eccles@icloud.com>
2019-09-17 14:09:48 -0600

    completed step 4

--- server/index.js
+++ server/index.js
@@ -2,7 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const session = require('express-session')
 const app = express()
+const checkForSession = require('./middlewares/checkForSession')
+const { SERVER_PORT, SESSION_SECRET } = process.env
 
 app.use(express.json())
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+app.use(checkForSession)
 
 app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} on station!`))
