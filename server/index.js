7d42632a4e2b6ab27373ab3cc91a0c479599a4bf
Ben Eccles <ben.g.eccles@icloud.com>
2019-09-17 14:43:36 -0600

    completed step 7

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const session = require('express-session')
 const app = express()
 const checkForSession = require('./middlewares/checkForSession')
+const auth = require('./controllers/authController')
 const swag = require('./controllers/swagController')
 const { SERVER_PORT, SESSION_SECRET } = process.env
 
@@ -16,5 +17,10 @@ app.use(checkForSession)
 
 //GET
 app.get('/api/swag', swag.read);
+
+app.post('/api/login', auth.login)
+app.post('/api/register', auth.register)
+app.post('/api/signout', auth.signout)
+app.get('/api/user', auth.getUser)
 
 app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} on station!`))
