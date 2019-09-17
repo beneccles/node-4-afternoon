d5bfae6b70f9dd894bd4506f7bbffba490947f33
Ben Eccles <ben.g.eccles@icloud.com>
2019-09-17 15:27:16 -0600

    step 11 completed.

--- server/index.js
+++ server/index.js
@@ -1,19 +1,27 @@
+// Server Setup Required
 require('dotenv').config()
 const express = require('express')
 const session = require('express-session')
 const app = express()
 const checkForSession = require('./middlewares/checkForSession')
+
+// Controller Files
 const auth = require('./controllers/authController')
 const swag = require('./controllers/swagController')
 const cart = require('./controllers/cartController')
+const search = require('./controllers/searchController')
 const { SERVER_PORT, SESSION_SECRET } = process.env
 
+// Middleware
 app.use(express.json())
 app.use(session({
     secret: SESSION_SECRET,
     resave: false,
     saveUninitialized: true
 }))
+
+// Check the session, see if the user matches a previous one or 
+// register a new user.
 app.use(checkForSession)
 
 //GET
@@ -29,5 +37,8 @@ app.get('/api/user', auth.getUser)
 app.post('/api/cart/checkout', cart.checkout)
 app.post('/api/cart/:id', cart.add)
 app.delete('/api/cart/:id', cart.delete)
+
+//Search Endpoint
+app.get('/api/search', search.search)
 
 app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} on station!`))
