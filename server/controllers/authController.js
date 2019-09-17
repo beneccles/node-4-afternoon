6baa5dd27f45bf8ac982989f5e66d5544e9f11d8
Ben Eccles <ben.g.eccles@icloud.com>
2019-09-17 14:40:02 -0600

    completed step 6

---
+++ server/controllers/authController.js
@@ -0,0 +1,47 @@
+const users = require("../models/users")
+let id = 1
+
+module.exports = {
+    login: (req, res) => {
+        const { session } = req;
+        const { username, password } = req.body;
+
+        // Check to see if the user already exists within our users.js file
+        // Compare the entered user and password against whats already in the system.
+        // If the comparsion fails, user will be empty, causing to be defined as falsy.
+        const user = user.find(user => user.username === username && user.password === password)
+
+        // If user is truthy, assign the user to the session. Otherwise, let the user know they are unauthorized.
+        if (user) {
+            session.user.username = user.username;
+            res.status(200).send(session.user);
+        } else {
+            res.status(500).send('Unauthorized.')
+        }
+
+    },
+    register: (req, res) => {
+        const { session } = req;
+        const { username, password } = req.body;
+
+        // Push the new user onto the array stored in users.js
+        users.push({ id, username, password });
+        id++;
+
+        // Assign the username to the session object
+        session.user.username = username;
+
+        // Return the call with the session info created.
+        res.status(200).send(session.user)
+    },
+    signout: (req, res) => {
+        // Destroy the info after the session is done, so that it cannot be used by an unauthorized party.
+        req.session.destroy();
+        res.status(200).send(req.session);
+    },
+    getUser: (req, res) => {
+        // Return the current session user.
+        const { session } = req;
+        res.status(200).send(session.user)
+    }
+}
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const session = require('express-session')
 const app = express()
 const checkForSession = require('./middlewares/checkForSession')
+const swag = require('./controllers/swagController')
 const { SERVER_PORT, SESSION_SECRET } = process.env
 
 app.use(express.json())
@@ -12,5 +13,8 @@ app.use(session({
     saveUninitialized: true
 }))
 app.use(checkForSession)
+
+//GET
+app.get('/api/swag', swag.read);
 
 app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} on station!`))
